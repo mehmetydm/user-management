ba9d82c2a3f3a078daecad08d850eefdd3b59a40
mehmet yıldırım <mehmetydm@gmail.com>
2023-02-02 16:56:07 +0300

    first commit.. microservices, classes and configs added..

---
+++ user.cmd.api/src/test/java/com/nichelush/user/cmd/api/UserCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nichelush.user.cmd.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = UserCommandApplicationTests.class)
+class UserCommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user.query.api/src/test/java/com/nichelush/user/query/api/UserQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nichelush.user.query.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = UserQueryApplicationTests.class)
+class UserQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
